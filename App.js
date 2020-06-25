b8085138b71bed1b947d12fc344c4d48affdeafb
Kamil Cichuta <kcichuta@gmail.com>
2020-06-25 14:49:33 +0200

    Bottom tabs created

--- App.js
+++ App.js
@@ -2,13 +2,37 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {
   SafeAreaView,
+  View,
+  Text,
 } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'red' }} />
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
