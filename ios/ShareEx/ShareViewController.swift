ff1f76b099327a3686166f801357338d5963b5be
Kamil Cichuta <kcichuta@gmail.com>
2020-07-20 14:36:42 +0200

    iOS: Get and save url

--- ios/ShareEx/ShareViewController.swift
+++ ios/ShareEx/ShareViewController.swift
@@ -14,6 +14,31 @@ class ShareViewController: UIViewController {
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     navigationController?.setNavigationBarHidden(true, animated: false)
+    setupInitialComponentValues()
+  }
+  
+  private func setupInitialComponentValues() {
+    if let item = extensionContext?.inputItems.first as? NSExtensionItem {
+      if let attachments = item.attachments {
+            for attachment: NSItemProvider in attachments {
+                if attachment.hasItemConformingToTypeIdentifier("public.url") {
+                  attachment.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (url, error) in
+                        if let shareURL = url as? URL {
+                          let userDefaults = UserDefaults(suiteName: "group.fakehunter.share")
+                          userDefaults?.set(shareURL.absoluteString, forKey: "shareUrl")
+                          userDefaults?.synchronize()
+                        }
+                    })
+                } else if attachment.hasItemConformingToTypeIdentifier("public.plain-text") {
+                  attachment.loadItem(forTypeIdentifier: "public.plain-text", options: nil, completionHandler: { url, error in
+                      let userDefaults = UserDefaults(suiteName: "group.fakehunter.share")
+                      userDefaults?.set(url, forKey: "shareUrl")
+                      userDefaults?.synchronize()
+                  })
+               }
+            }
+        }
+    }
   }
 }
 
